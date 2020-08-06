9e84ada09070ecda9fd5188ecbc062f8393690fe
secure-system-2319 <arhci9494@gmail.com>
2020-08-06 00:43:43 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,10 +1,5 @@
-def say_hello 
-  puts "Hello Ruby Programmer!"
+def say_hello (name="Ruby Programmer")
+  puts "Hello, #{name}!"
 end
-def say_hello (Gabriela)
-  puts "Hello, #{Gabriela}"
-end
-def say_hello (name = "Ruby Programmer")
-  puts "Hello, #{Gabriela}"
-end
-
+say_hello ("Gabriela")
+Hello, Gabriela!
